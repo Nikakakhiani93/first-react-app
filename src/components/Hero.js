72f1e06893fa60e9b900f80c8fad8c11ee7877e3
Nikakakhiani93 <79855486+Nikakakhiani93@users.noreply.github.com>
2022-05-31 00:52:10 +0400

    add some code refactor

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ import MainPicture from '../images/main-pic.svg';
 export default function Hero() {
   return (
     <main className="hero">
-      <img src={MainPicture} alt="MainPicture" />
+      <img src={MainPicture} alt="MainPicture" className="main-pic" />
       <h1>Online Experiences</h1>
       <p>
         Join unique interactive activities led by one-of-a-kind hosts—all
